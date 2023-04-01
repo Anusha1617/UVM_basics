60a1c868f03ed3411c58a6068f5112c830c7b010
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-01 19:03:48 +0530

    Create Mem_op_mo.sv

---
+++ ETH_EXAM/Mem_op_mo.sv
@@ -0,0 +1,13 @@
+class Mem_op_mo extends uvm_monitor;
+
+//------FACTORY Registration----------------//
+	`uvm_component_utils(Mem_op_mo)
+
+
+// component constructor 
+function new(string name,uvm_component parent);
+		super.new(name,parent);
+endfunction
+
+
+endclass
