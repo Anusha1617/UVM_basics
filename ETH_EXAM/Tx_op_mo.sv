c72920891afcdc3bb211aecf00a2e97933a2c761
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-01 19:12:24 +0530

    Create Tx_op_mo.sv

---
+++ ETH_EXAM/Tx_op_mo.sv
@@ -0,0 +1,13 @@
+class Tx_op_mo extends uvm_monitor;
+
+
+//------FACTORY Registration----------------//
+	`uvm_component_utils(Tx_op_mo)
+
+// component constructor 
+function new(string name,uvm_component parent);
+		super.new(name,parent);
+endfunction
+
+
+endclass
