66a2afa668640302eca2d9afc8779a8d7bc6f96c
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-01 19:07:23 +0530

    Create Rx_ip_mo.sv

---
+++ ETH_EXAM/Rx_ip_mo.sv
@@ -0,0 +1,13 @@
+class Rx_ip_mo extends uvm_monitor;
+
+
+//------FACTORY Registration----------------//
+	`uvm_component_utils(Rx_ip_mo)
+
+// component constructor 
+function new(string name,uvm_component parent);
+		super.new(name,parent);
+endfunction
+
+
+endclass
