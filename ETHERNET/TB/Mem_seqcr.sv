4c07391654aef6448da85f7cfad5c7a87995b0de
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-24 16:57:34 +0530

    Create Mem_seqcr.sv

---
+++ ETHERNET/TB/Mem_seqcr.sv
@@ -0,0 +1,10 @@
+//`include "Mem_seq_item.sv"
+
+class Mem_seqcr extends uvm_sequencer #(Mem_seq_item);
+  `uvm_component_utils(Mem_seqcr)
+  
+  function new(string name = "Mem_seqcr", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+    
+endclass
