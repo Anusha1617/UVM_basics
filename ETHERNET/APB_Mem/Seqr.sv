5baddb14f3b8d155e6791979d4e61ec0e41e8678
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-24 22:22:17 +0530

    Delete ETHERNET/APB_Mem directory

--- ETHERNET/APB_Mem/Seqr.sv
+++
@@ -1,9 +0,0 @@
-class Mem_seqcr extends uvm_sequencer #(Mem_seq_item);
-  `uvm_component_utils(Mem_seqcr)
-  
-  function new(string name = "Mem_seqcr", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction
-  
-  
-endclass
