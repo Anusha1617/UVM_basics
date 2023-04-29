bce00f7f849d7c082933f91414830515bc092084
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-29 19:18:38 +0530

    Update Tx_Driver.sv

--- ETHERNET/TB/Tx_Driver.sv
+++ ETHERNET/TB/Tx_Driver.sv
@@ -1,4 +1,3 @@
-
 class Tx_driver extends uvm_driver#(Tx_Seq_Item);
 
 //------FACTORY Registration----------------//
@@ -31,7 +30,7 @@ task  run_phase(uvm_phase phase);
         fork 
         	drive;
         	drive_int_source;
-        join_any
+        join
 
 
 endtask
@@ -39,24 +38,29 @@ endtask
 task drive_int_source;
 forever begin
 @(h_Ethernet_Intf.host_cb_driver) if(h_Ethernet_Intf.int_o) begin
-$display("DRV i=%0d",i);
+//$display("DRV i=%0d",i);
+            //read_int_source;
 			i++;
 			h_Ethernet_Intf.host_cb_driver.prstn_i<=1;
 			h_Ethernet_Intf.host_cb_driver.psel_i<=1;		//Setup stae s=1 e=0
-			h_Ethernet_Intf.host_cb_driver.penable_i<=0;			
+			h_Ethernet_Intf.host_cb_driver.penable_i<=0;
 			h_Ethernet_Intf.host_cb_driver.pwrite_i<=1;
 			h_Ethernet_Intf.host_cb_driver.paddr_i<='h04;
-			h_Ethernet_Intf.host_cb_driver.pwdata_i<='habcd;
-			
+			h_Ethernet_Intf.host_cb_driver.pwdata_i<='habcf;
+
 			@(h_Ethernet_Intf.host_cb_driver);		//access state  s=1 e=1
 			h_Ethernet_Intf.host_cb_driver.penable_i<=1;
-			
+
 				wait(h_Ethernet_Intf.pready_o);
-				                            @(h_Ethernet_Intf.host_cb_driver);		//acess state  s=1 e=1						
+				                            @(h_Ethernet_Intf.host_cb_driver);		//acess state  s=1 e=1
 			h_Ethernet_Intf.host_cb_driver.psel_i<=0;		//Setup stae s=1 e=0
-			h_Ethernet_Intf.host_cb_driver.penable_i<=0;						
-			$display("DRV TX i=%0d ,txbdnum=%0d",i,h_db.TX_BD_NUM);
-			if(i==h_db.TX_BD_NUM) break;
+			h_Ethernet_Intf.host_cb_driver.penable_i<=0;
+			`uvm_info("PACKETS RXED",$sformatf("DRV no of PAcket recieved =%0d ,txbdnum=%0d",i,h_db.TX_BD_NUM),UVM_HIGH)
+			if(i==h_db.TX_BD_NUM) begin
+			     h_db.default_values;
+			     i=0;
+//			 break;
+			 end
 			end
 			end
 endtask
@@ -64,39 +68,47 @@ endtask
 
 task drive;
 
-        h_Ethernet_Intf.Tx_cb_driver.MCrS<=0;        
+        h_Ethernet_Intf.Tx_cb_driver.MCrS<=0;
 
         forever @(h_Ethernet_Intf.Tx_cb_driver)
         begin
                seq_item_port.get_next_item(req);
                wait(h_Ethernet_Intf.MTxEn /*|| h_Ethernet_Intf.int_o*/);
-              if(h_Ethernet_Intf.MTxEn)  
-                begin 
+              if(h_Ethernet_Intf.MTxEn)
+                begin
                 	forever@(h_Ethernet_Intf.Tx_cb_driver)
                 	begin
 				h_Ethernet_Intf.Tx_cb_driver.MCrS<=1;
                    		if(h_Ethernet_Intf.MTxEn==0) begin
                 						 h_Ethernet_Intf.Tx_cb_driver.MCrS<=0;
-//                						 $display("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-                						 break; 
+                						 break;
                 						  end
                    	end
                	end
-               	/*else  begin   
-             	
-           	                						 h_Ethernet_Intf.Tx_cb_driver.MCrS<=0;
-           	                						 drive_int_source;
-           	                						            	                						 $display(" before RD=%p",h_db.RD);
-           	                						 h_db.RD[i]=0;
-           	                						 $display("RD=%p",h_db.RD);
-           	                						 i++;
-           	                						 
-           	                						 
-               	
-               	   end*/
                 seq_item_port.item_done();
         end
 
+
+endtask
+
+task read_int_source;
+			h_Ethernet_Intf.host_cb_driver.prstn_i<=1;
+			h_Ethernet_Intf.host_cb_driver.psel_i<=1;		//Setup stae s=1 e=0
+			h_Ethernet_Intf.host_cb_driver.penable_i<=0;
+			h_Ethernet_Intf.host_cb_driver.pwrite_i<=0;
+			h_Ethernet_Intf.host_cb_driver.paddr_i<='h04;
+			h_Ethernet_Intf.host_cb_driver.pwdata_i<='habcf;
+			
+			@(h_Ethernet_Intf.host_cb_driver);		//access state  s=1 e=1
+			h_Ethernet_Intf.host_cb_driver.penable_i<=1;
+			
+				wait(h_Ethernet_Intf.pready_o);
+				                            @(h_Ethernet_Intf.host_cb_driver);		//acess state  s=1 e=1						
+                                          //  if(h_Ethernet_Intf.prdata_o)   
+			h_Ethernet_Intf.host_cb_driver.psel_i<=0;		//Setup stae s=1 e=0
+			h_Ethernet_Intf.host_cb_driver.penable_i<=0;						
+	//		$display("DRV TX i=%0d ,txbdnum=%0d",i,h_db.TX_BD_NUM);
+		//	if(i==h_db.TX_BD_NUM) break;
 endtask
 
 
