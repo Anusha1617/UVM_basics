9335f9b542fce549e4aa29914100c2bb8688f54f
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-29 19:07:49 +0530

    Update all_signals_naming_convention.sv

--- ETHERNET/TB/all_signals_naming_convention.sv
+++ ETHERNET/TB/all_signals_naming_convention.sv
@@ -1,3 +1,44 @@
-mcs_dv05_ethernet_project_assertions.sv
-mcs_dv05_ethernet_project_top_config.sv
-mcs_dv05_ethernet_project_top.sv
+//----------APB SLAVE -------//
+pclk_i
+prstn_i
+paddr_i
+pwdata_i
+psel_i
+pwrite_i
+penable_i
+
+prdata_o
+pready_o
+
+//------COMMON SIGNALS ------//
+
+int_o
+
+//----- APB MASTER ----------//
+
+m_paddr_o
+m_pwdata_o
+m_psel_o
+m_pwrite_o
+m_penable_o
+
+m_pready_i
+m_prdata_i
+
+
+//------TX-------//
+MTxClk
+MTxD
+MTxEn   
+MTxErr
+
+//------COMMON SIGNALS ------//
+MCrS
+
+//------RX-------//
+MRxClk
+MRxDV
+MRxD
+MRxErr
+
+
