fa90d6eac4d13453526f6aaabff31d20bde0128c
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-29 19:18:04 +0530

    Update Tx_Seqr.sv

--- ETHERNET/TB/Tx_Seqr.sv
+++ ETHERNET/TB/Tx_Seqr.sv
@@ -1,1 +1,13 @@
 
+class Tx_seqr extends uvm_sequencer #(Tx_Seq_Item);
+
+
+//------FACTORY Registration----------------//
+	`uvm_component_utils(Tx_seqr)
+
+// component constructor 
+function new(string name,uvm_component parent);
+		super.new(name,parent);
+endfunction
+
+endclass
