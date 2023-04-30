13050859301f3918b5c12806a741df6ad401bae2
matam basavaraju <57475017+basavaraju496@users.noreply.github.com>
2023-04-30 14:43:07 +0530

    Create 1_display_uvm_info.sv

---
+++ Reporting_Mechanism/1_display_uvm_info.sv
@@ -0,0 +1,23 @@
+`include "uvm_macros.svh" ///`uvm_info
+import uvm_pkg::*;
+ // imports all  uvm base classes 
+module tb;
+  initial begin
+    #50;
+    //-----------`uvm_info(ID,MSG,VERBOSITY)--------------------------//
+
+    `uvm_info("TB_TOP","Hello World", UVM_LOW);
+    // tells about 
+    /*
+    1. File name
+    2. Line number 
+    3. Time 
+    4. module or class name
+    5. Message 
+    */ 
+     $display("Hello World with Display");
+  end
+endmodule
+
+# UVM_INFO top.sv(17) @ 50: reporter [TB_TOP] Hello World
+# Hello World with Display
